9c6f4f9273fc4166843a6b1a6162e75f1c9afa2a
aliberkayberber <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>
2023-01-21 13:52:26 +0300

    developing Home Screen
    developing Home Screen

---
+++ IGDb/Screen/FavoriteScreen/Model/FavoriteScreenViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FavoriteScreenViewModel.swift
+//  IGDb
+//
+//  Created by Ali Berkay BERBER on 21.01.2023.
+//
+
+import Foundation
+
+protocol FavoriteScreenViewModelProtocol {
+    
+}
+
+
